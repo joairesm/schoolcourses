4cfd30c892a18e6ec6fb6c8428139c96266e0ad0
Joao Aires <jo.airesm@gmail.com>
2019-01-19 16:16:37 +0100

    set-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CourseComponent } from './components/course/course.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CoursesComponent,
+    CourseComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
